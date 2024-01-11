0732c7da33be1e54c5ec1c91a51f3ed33e1adb3e
AlanGanser <Alanganser1@gmail.com>
2024-01-11 08:26:02 -0700

    troubleshooting 3

--- nextjs-blog/app/api/users/[userId]/donations/[donationId]/route.ts
+++ nextjs-blog/app/api/users/[userId]/donations/[donationId]/route.ts
@@ -13,7 +13,7 @@ export async function GET(request: NextRequest, { params }: { params: { donation
 
 export async function PUT(request: NextRequest, { params }: { params: { donationId: string } }) {
     const { donationId } = params;
-    const { address, date }: { address: string; date: Date } = await request.json();
+    const { address, date }: { address: string; date: string } = await request.json();
     await prisma.donation.update({
         where: {
             id: donationId,
